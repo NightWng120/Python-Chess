7938950e36d6cfcead76c3c813831245f92f9d03
NightWng120 <treverholmes9@gmail.com>
2024-04-30 18:31:28 -0400

    removed old code comment

--- testgame.py
+++ testgame.py
@@ -1,31 +1,5 @@
 import player, board,pdb, copy, game
 
-# board = board.Board()
-# player1 = player.Player(True)
-# player2 = player.Player(False)
-# 
-# piecesAll = player1.pieces
-# [piecesAll.append(i) for i in player2.pieces]
-# board.update(piecesAll)
-# board.printBoard()
-# 
-# # [6,0] -> [5, 2]
-# userIn = "    G1    , F3               "
-# userIn = userIn.split(",")
-# for i, val in enumerate(userIn):
-#     userIn[i] = val.strip()
-# 
-# print(userIn)
-# 
-# boolean = player1.movePiece(player2, player1.pieces[player1.index(board.inputToPos(userIn[0]))], board.inputToPos(userIn[1]))
-# # boolean = player1.movePiece(player1.pieces[player1.index([6,0])], player2, [7, 2])
-# print(boolean)
-# 
-# piecesAll = player1.pieces
-# [piecesAll.append(i) for i in player2.pieces]
-# board.update(piecesAll)
-# board.printBoard()
-
 user = True
 gameboard = board.Board()
 white = player.Player(True)
@@ -80,7 +54,7 @@ for i in data:
     piecesAll.clear()
 
     print(f"stalemate: {black.stalemate(white.possibleMoves, white)}")
-    # print(f"hasWon: {game.hasWon(white, black, user)}")
+    print(f"hasWon: {game.hasWon(white, black, user)}")
     # print(f"Player {players[user]} is in check: {colors[user].kingP.check(colors[not user].possibleMoves)}")
     # print(f"{colors[user].kingP.moves(colors[not user].possibleMoves)}")
     # print(f"Player: {user}")
